9fe7a5e1651c546e7012272be970c68fdbbf9a7d
rktst110 <84590431+rktst110@users.noreply.github.com>
2024-02-06 22:30:09 +0530

    Update main.js

--- blgrClck/main.js
+++ blgrClck/main.js
@@ -6,8 +6,9 @@ var runAccordingToSepcificConditions = true
 
 //var url = 'https://api.apify.com/v2/acts/kbvhuuvcccch~puppeteer-scraper/runs?token='+token
 //var whichActorToRun = 'https://api.apify.com/v2/acts/kbvhuuvcccch~puppeteer-scraper/runs'
-var whichActorToRun = 'https://api.apify.com/v2/acts/fghrh65h565~my-puppeteer-scraper/runs'
+//var whichActorToRun = 'https://api.apify.com/v2/acts/fghrh65h565~my-puppeteer-scraper/runs'
 //var whichActorToRun = 'https://api.apify.com/v2/acts/fd564df564df~apify-sdk-js-jan-2024/runs'
+var whichActorToRun = 'https://api.apify.com/v2/acts/zkkihgdvhf~apify-sdk-js-jan-2024/runs'
 
 async function startRunningActorProcess() {
 	
